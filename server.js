2fbbe58789c2a48eba4ae159835174f7098d3613
Charan <charan@nextops.in>
2026-01-06 20:06:17 +0530

    port number revereted back to 8080

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http')
 
-const port = 8000
+const port = 8080
 const server = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'})
   response.write('Hello World\n')
