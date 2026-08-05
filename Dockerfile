c84742bf95e43c08ae9d325a0ffa4d32e6443488
Charan Baisetty <charan@nextops.in>
2026-08-05 07:16:25 +0530

    revertd back port 80

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM node:15-alpine
 
 COPY . /src
 RUN cd /src && npm install
-EXPOSE 8080
+EXPOSE 80
 CMD ["node", "/src/server.js"]
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 
 // updated to use port 80 for better compatibility with Azure App Service
-const port = 8080
+const port = 80
 const server = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'})
   response.write('Hello World\n')
